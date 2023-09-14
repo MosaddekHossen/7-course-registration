b3473091af1adf388227418eacfffefb8ea66120
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-09-14 23:47:31 +0600

    Dynamic display of card information

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -21,9 +21,9 @@ const Cards = () => {
                             <div key={item.id} className="p-5 bg-white rounded-lg">
                                 <img src={item.img} alt="" />
                                 <h3 className="text-[#1C1B1B] font-semibold text-[18px] pt-4">{item.course}</h3>
-                                <p className="py-3">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                <p className="py-3">{item.details}</p>
                                 <div>
-                                    <h6 className="flex justify-evenly"><span>0</span> <span>Price : 15000</span> <span>0</span> <span className="text-right">Credit : 2hr</span></h6>
+                                    <h6 className="flex justify-evenly"><span>0</span> <span>Price : {item.price}</span> <span>0</span> <span className="text-right">Credit : {item.credit}hr</span></h6>
                                 </div>
                                 <div className="text-center mt-3">
                                     <button className="bg-[#2F80ED] text-white px-8 py-2 w-full text-[18px] rounded-lg">Select</button>
