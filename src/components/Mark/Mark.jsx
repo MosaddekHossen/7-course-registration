45d21186de35c6d65f4d0c2654279f84e34bc4ca
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-09-14 23:34:09 +0600

    Bookmarks section layout is created

--- src/components/Mark/Mark.jsx
+++ src/components/Mark/Mark.jsx
@@ -1,9 +1,20 @@
 
-
 const Mark = () => {
     return (
         <div>
-            <h2 className="bg-green-200">Lorem Thecorporis, ducimus suscipit soluta deserunt ratione dolore rerum velit, minus ab ipsam esse quis animi. Ducimus nulla, maiores quae quod tenetur, soluta nobis unde sit omnis corrupti odit accusamus itaque possimus voluptatibus aliquam tempore?</h2>
+            <div className="p-5 bg-white rounded-lg">
+                <div className="border-b-2">
+                    <h3 className="text-[#2F80ED] font-semibold text-[18px] py-4">Credit Hour Remaining 7 hr</h3>
+                </div>
+                <h3 className="text-[#1C1B1B] font-bold text-[20px] pt-4">Course Name</h3>
+                <p>p</p>
+                <div className="border-y-2 mt-4">
+                    <h3 className="font-semibold text-[18px] py-3">Total Credit Hour : 13</h3>
+                </div>
+                <div className="border-b-2">
+                    <h3 className="font-semibold text-[18px] py-3">Total Price : 48000 USD</h3>
+                </div>
+            </div>
         </div>
     );
 };
